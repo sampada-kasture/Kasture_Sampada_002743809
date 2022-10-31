b153b0bba2debfbf855b1ee1213c27cc36576672
sampada-kasture <kasture.s@northeastern.edu>
2022-10-31 10:53:53 +0530

    Update 2

---
+++ assignment2/src/model/Residence.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Subhash
+ */
+public class Residence extends Community{
+    protected String Residence;
+
+    public String getResidence() {
+        return Residence;
+    }
+
+    public void setResidence(String Residence) {
+        this.Residence = Residence;
+    }
+    
+}
